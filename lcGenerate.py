68eae65ecbe5be49aa2679489fe850fddcb39063
Aleix Claramunt <aleixcm@gmail.com>
2018-04-23 20:43:52 +0200

    added some generated and new dataset

--- lcGenerate.py
+++ lcGenerate.py
@@ -9,12 +9,12 @@ b = [1]
 c = [2]
 
 # Samples per Category
-aSamples = 8000
-bSamples = 8000
-cSamples = 8000
+aSamples = 16000
+bSamples = 16000
+cSamples = 16000
 def main():
     sequence = c *cSamples + b*bSamples + a*aSamples
-    file00 = open(os.path.join('corpus','localTrain','lc_gen1_24000.txt'), 'w')
+    file00 = open(os.path.join('corpus','localTrain','lc_gen3_48000.txt'), 'w')
     for item in sequence:
         file00.write("%s,\n" % item)
 
