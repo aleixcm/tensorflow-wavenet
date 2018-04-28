068196695d1dad95e28b46b75208b7b5568e3d50
aleixcm <aleix@Aleixs-MacBook-Pro.local>
2018-04-28 11:24:44 +0200

    aa

--- lcGenerate.py
+++ lcGenerate.py
@@ -9,12 +9,12 @@ b = [1]
 c = [2]
 
 # Samples per Category
-aSamples = 16000
-bSamples = 16000
-cSamples = 16000
+aSamples = 24000
+bSamples = 24000
+cSamples = 24000
 def main():
-    sequence = c *cSamples + b*bSamples + a*aSamples
-    file00 = open(os.path.join('corpus','localTrain','lc_gen3_48000.txt'), 'w')
+    sequence = a *aSamples + b*bSamples + c*cSamples
+    file00 = open(os.path.join('corpus','localTrain','lc_gen4_72000.txt'), 'w')
     for item in sequence:
         file00.write("%s,\n" % item)
 
